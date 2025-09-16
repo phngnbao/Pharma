cbb850aae3534db70bb7d7e0e38ae66f58da8800
phngnbao <phnguyenbao24@gmail.com>
2025-09-16 15:17:48 +0700

    Update index_backup.js

--- pharma-backend/index_backup.js
+++ pharma-backend/index_backup.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const {
     verifyFirebaseToken,
